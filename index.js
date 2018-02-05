e0960e512690fc522b5f52ff99ad8efac17f241e
Luke Kozak <kozakluke@gmail.com>
2018-02-05 12:32:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 #!/usr/bin/env node
-'use strict';
+const fs 	 = require('fs-extra'),
+	  {exec} = require('child_process');
 
-const fs = require('fs-extra');
-const exec = require('child_process').exec;
-
-const pwd  = process.env.PWD,
-	  name = process.argv[2];
+const pwd 	   = process.env.PWD,
+	  name 	   = process.argv[2],
+	  template = process.argv[3];
 if (!name)
-	return console.error('Template require a name');
+	return console.error('Usage: react-template-ts2 <project-name> <--redux? | --router?>');
 
-fs.copySync(__dirname + '/Template', `${pwd}/${name}`);
+switch (template)
+{
+	case '--redux' :
+		copyTemplate('template-redux');
+		npmInstall();
+		break;
+	case '--router' :
+		copyTemplate('template-router');
+		npmInstall();
+		break;
+	default :
+		copyTemplate('template');
+		npmInstall();
+}
 
-const env = {FORCE_COLOR: '1', NPM_CONFIG_COLOR: 'always'};
+function copyTemplate(template)
+{
+	fs.copySync(`${__dirname}/${template}`, `${pwd}/${name}`);
+}
 
-
-const child = exec(`cd ${pwd}/${name} &&\
-                    npm install`,
-                   {env});
-child.stdout.on('data', console.log);
-child.stderr.on('data', console.log);
-
-
-return;
-/*
-var process = exec(`cd ${server} &&\
-                      git add . &&\
-                      git commit -m "Update: ${project}." &&\
-                      git push origin`);
-process.stdout.on('data', console.log);
-process.stderr.on('data', console.log);
-
-
-return;
-const execSync = require('child_process').execSync;
-
-var d = execSync('pwd');
-console.log(process.argv, d.toString(), __dirname);
-console.log(process.env.PWD);
-*/
+function npmInstall()
+{
+	const env = {
+		NPM_CONFIG_COLOR: 'always'
+	};
+	const child = exec(`cd ${pwd}/${name} &&\
+	                    npm install`,
+	                   {env});
+	child.stdout.on('data', console.log);
+	child.stderr.on('data', console.log);
+}
