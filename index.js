a6604223f405b8f547ee5f93c36c133eb85df472
Luke Kozak <kozakluke@gmail.com>
2018-07-17 13:32:26 +0200

    Style tabs to spaces

--- index.js
+++ index.js
@@ -2,38 +2,38 @@
 const fs = require('fs-extra');
 const {exec} = require('child_process');
 
-const pwd 	    = process.env.PWD,
-       name 	= process.argv[2],
-	   template = process.argv[3];
+const pwd = process.env.PWD,
+      name = process.argv[2],
+      template = process.argv[3];
 if (!name)
-	return console.error('Usage: react-template-ts <project-name> <--redux? | --router?>');
+    return console.error('Usage: react-template-ts <project-name> <--redux? | --router?>');
 
 switch (template)
 {
-	case '--redux' :
-		copyTemplate('redux');
-		npmInstall();
-		break;
-	case '--router' :
-		copyTemplate('router');
-		npmInstall();
-		break;
-	default :
-		copyTemplate('default');
-		npmInstall();
+    case '--redux' :
+        copyTemplate('redux');
+        npmInstall();
+        break;
+    case '--router' :
+        copyTemplate('router');
+        npmInstall();
+        break;
+    default :
+        copyTemplate('default');
+        npmInstall();
 }
 
 function copyTemplate(template)
 {
-	fs.copySync(`${__dirname}/templates/${template}`, `${pwd}/${name}`);
+    fs.copySync(`${__dirname}/templates/${template}`, `${pwd}/${name}`);
 }
 
 function npmInstall()
 {
-	const env = {
-		NPM_CONFIG_COLOR: 'always'
-	};
-	const child = exec(`cd ${pwd}/${name} && npm install`, {env});
-	child.stdout.on('data', console.log);
-	child.stderr.on('data', console.log);
+    const env = {
+        NPM_CONFIG_COLOR: 'always'
+    };
+    const child = exec(`cd ${pwd}/${name} && npm install`, {env});
+    child.stdout.on('data', console.log);
+    child.stderr.on('data', console.log);
 }
