b0f5b1487c3502af9984aa4e469dc88f4ef2f73d
Luke Kozak <kozakluke@gmail.com>
2018-07-18 08:23:22 +0200

    Fix chunk filename is chunkhash in production

--- templates/default/webpack.config.prod.js
+++ templates/default/webpack.config.prod.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js',
-        chunkFilename: '[hash:10].js'
+        chunkFilename: '[chunkhash].js'
     },
     mode: 'production',
     module: {
