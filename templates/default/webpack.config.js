4ec69e14890f3723b7deab0960bb123972b6211b
Luke Kozak <kozakluke@gmail.com>
2018-09-21 19:58:38 +0200

    Add public path in webpack config

--- templates/default/webpack.config.js
+++ templates/default/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, 'public'),
+        publicPath: '/',
         filename: 'bundle.js',
         chunkFilename: '[name].js'
     },
--- templates/default/webpack.config.prod.js
+++ templates/default/webpack.config.prod.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, 'public'),
+        publicPath: '/',
         filename: 'bundle.js',
         chunkFilename: '[chunkhash].js'
     },
