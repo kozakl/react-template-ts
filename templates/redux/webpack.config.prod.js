fd1afb53b969b183cb0f501e1d1534e1b7418b3b
Luke Kozak <kozakluke@gmail.com>
2018-11-04 10:06:25 +0100

    Hash css modules in production

--- templates/redux/webpack.config.prod.js
+++ templates/redux/webpack.config.prod.js
@@ -40,7 +40,7 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             modules: true,
-                            localIdentName: '[local]__[hash:base64:10]'
+                            localIdentName: '[hash:base64:10]'
                         }
                     },
                     'postcss-loader'
