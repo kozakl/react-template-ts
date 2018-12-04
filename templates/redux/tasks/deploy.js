22a5cade6b799e5f0ed97bab08f7b366f65375b7
Luke Kozak <kozakluke@gmail.com>
2018-12-04 15:31:21 +0100

    Fix deploy task

--- templates/redux/tasks/deploy.js
+++ templates/redux/tasks/deploy.js
@@ -9,7 +9,7 @@ const server = process.argv[2],
 try {
     const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim(),
           tag = execSync('git tag').toString().length &&
-                execSync('git describe --tags --abbrev=0').toString().trim();
+                execSync('git describe --tags --always').toString().trim();
     if (branch === 'master' && tag)
         deploy(join(name, tag));
     else
